3d1acc276c6252e2cd93cedf4718caa68b29a4cd
Faith Wavinya <45032405+faith-wm@users.noreply.github.com>
2025-03-02 22:34:46 -0500

    Update grpo_training.py

--- grpo_training.py
+++ grpo_training.py
@@ -1,64 +1,85 @@
+import pandas as pd
 import datasets
 import re
 import time
-from transformers import Trainer, TrainingArguments, AutoTokenizer, AutoModelForCausalLM, DataCollatorForSeq2Seq, BitsAndBytesConfig, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from peft import LoraConfig
 from torch.backends import cuda
-from torch import bfloat16, float16
+from torch import bfloat16
 import os
-import deepspeed
-from torch.utils.data import DataLoader
-import argparse
-# from mpi4py import MPI
-import torch
-from torch.utils.data import DataLoader, RandomSampler
-from trl import SFTConfig, SFTTrainer
 import random
-from trl import GRPOConfig, GRPOTrainer, OnlineDPOConfig, OnlineDPOTrainer
+from trl import GRPOConfig, GRPOTrainer
 
-# 
 random.seed(100)
 
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 cuda.matmul.allow_tf32 = True
 
 
-
-# import wandb
-
-from dataprep import hpo_data, reward_length, reward_hpo_distance
-
-def reward_len(completions, **kwargs):
-    return [-abs(20 - len(completion)) for completion in completions]
-
-
-def main(name):
-    model_id='Llama3/Meta-Llama-3.1-8B-Instruct/'
+def reward_length(completions,ground_truth, **kwargs):
+    try: 
+        completions = [completion[0]["content"] for completion in completions]
+        completions_length=[len(completion.split(' ')) for completion in completions]
+        length_score= [
+            1 - abs(400 - length) / 200 if 200 <= length <= 600 
+            else -abs(400 - length) / 50
+            for length in completions_length 
+        ]
+    except:
+        length_score= [0] * len(completions)
     
-    tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=False)
-    tokenizer.pad_token = tokenizer.eos_token
+    # save_samples(completions,ground_truth)
+    return length_score
 
-    train_data = datasets.Dataset.from_pandas(hpo_data(tokenizer)).shuffle(seed=42)
-    print(train_data)
+
+def save_samples(completions,ground_truth):
+    for completion,gt in zip(completions,ground_truth):
+        if random.random() < 0.1:  # 1% chance to write samples into a file
+            os.makedirs("completion_samples", exist_ok=True)
+            log_file = os.path.join("completion_samples", "completion_samples.txt")
+            with open(log_file, "a") as f:
+                f.write(f"\n\n==============\n")
+                f.write(f"{completion}")
+    return
+
+
+def prepare_data():
+    """
+    File with columns prompt and ground_truth
+    """
+    file='train_data.csv'
+
+    df=pd.read_csv(file)
+    ## convert prompt to conversational format
+    df['prompt']=df['prompt'].apply(lambda x: [{"role": "user", "content": x}]) 
+    return df
+    
+
+
+def main(model_id):
+   
+    tokenizer = AutoTokenizer.from_pretrained(model_id,use_fast=False)
+    tokenizer.pad_token = tokenizer.eos_token
+    
+    train_data =datasets.Dataset.from_pandas(prepare_data()).shuffle(seed=42)
+
+    print(train_data)  
 
     timestamp = time.strftime("%Y%m%d_%H")
-    filename=re.search(r'(\d+B)', model_id).group(0)
-    out_dir = f'finetuned_models/{filename}_{timestamp}_{name}'
+    modelname=re.search(r'(\d+B)', model_id).group(0)
+    out_dir = f'{modelname}_{timestamp}'
     os.makedirs(out_dir, exist_ok=True)
 
-    ######
-    # wandb.init(project=f"{filename}_{timestamp}_{name}")
-    ######
 
     params = {
-    # "gradient_checkpointing_kwargs": {"use_reentrant": False},
     "output_dir": out_dir, 
-    "deepspeed": "single_node_8b.json",
+    "deepspeed": "deepspeed_config.json",
     "overwrite_output_dir": True,
     "seed": 42,
     "logging_strategy": "steps",
     "logging_steps": 1,
-    "learning_rate": 5e-7,
+    "learning_rate": 2e-5,
     "gradient_checkpointing": True,
     "per_device_train_batch_size": 1,
     "gradient_accumulation_steps": 1,
@@ -68,85 +89,31 @@ def main(name):
     "save_steps": 50,
     "save_on_each_node": False,
     "save_total_limit": 3,
-    "report_to": "tensorboard",#["tensorboard", "wandb"],
+    "report_to":"tensorboard",
     "push_to_hub": False,
-    # "optim": "adamw_torch",
-    # "max_prompt_length":5000,
+    "optim": "adamw_torch",
+    "max_prompt_length":5000,
     "max_completion_length":2000,
     "temperature":0.6,
-    # "num_generations":4,
+    "num_generations":2,
     "remove_unused_columns":False,
     "log_completions":True,
-    ####
-    # "missing_eos_penalty":1,
-    # "max_new_tokens":1000
-    ######
-    #  "use_vllm":True,
-    # "vllm_device":"cuda:7",
-    # "vllm_gpu_memory_utilization":0.3,
-    #  "num_generations":7,
-        # "max_length":20000,
-    # "vllm_max_model_len":20000,
-        # "warmup_ratio": 0.2, 
-          # "weight_decay": 0.05,' 
-          # # "num_train_epochs": 20, 
-              # "lr_scheduler_type": "cosine", 
-            # "max_grad_norm": 0.3,
-    # "do_eval":False,
     }   
 
 
     training_args = GRPOConfig(**params)
 
-    # params['rank']=128
-    # params['alpha']=256
-    params['setting']='GRPO'
-    params['train']=train_data
-    # params['test']=test_data
-
-    param_file_path = os.path.join(out_dir, "parameters.txt")
-    with open(param_file_path,'w') as parameters_file:
-        for k,v in params.items():
-            parameters_file.write(f'{k}:{v}\n')
-    print('Parameters written to file')
-
-    
-    # bnb_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=bfloat16, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type='nf4')
-    
-    # peft_config = LoraConfig(
-    #     r=params['rank'],
-    #     lora_alpha=params['alpha'],
-    #     lora_dropout=0.05,
-    #     bias="none",
-    #     task_type="CAUSAL_LM",
-    #     target_modules=['down_proj', 'gate_proj', 'o_proj', 'v_proj', 'up_proj', 'q_proj', 'k_proj']
-    #     )
-    
-
     model = AutoModelForCausalLM.from_pretrained(model_id,
         torch_dtype=bfloat16,
         low_cpu_mem_usage=True,
-        # quantization_config=bnb_config,
         )
-    
-
-    # model.gradient_checkpointing_enable()
-    # model = prepare_model_for_kbit_training(model)
-    # model.config.use_cache = False
-    # model = get_peft_model(model, peft_config)
-    # # print(model.print_trainable_parameters())
-
-    # model.config.use_cache = False
-    # model.config.pretraining_tp = 1
 
 
     trainer = GRPOTrainer(
         model=model,
         args=training_args,
         train_dataset=train_data,
-        reward_funcs=[reward_hpo_distance],
-        processing_class=tokenizer,
-        # peft_config=peft_config
+        reward_funcs=[reward_length],  # can pass multiple functions
     )
 
     trainer.train()
@@ -154,7 +121,9 @@ def main(name):
     print('Training DONE')
 
 
-main(name=f'llama_norm_only')
+if __name__ == "__main__":  
+    model_id='Meta-Llama-3.1-8B-Instruct/'
+    main(model_id)
 
 
 
